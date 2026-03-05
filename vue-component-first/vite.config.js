63c28d03399d27b4e5787140e5a2daa4636b24e4
iceicelychee <https://github.com/iceicelychee/Deploy-tests-online.git>
2026-03-05 16:28:52 +0800

    vercel 前端vite配置文件PORK修改

--- vue-component-first/vite.config.js
+++ vue-component-first/vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+// import process from 'node:process'
 const port = process.env.PORT || 9000;
 
 // https://vite.dev/config/
@@ -19,15 +19,16 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: port,
+    // 开发调试端口
+    port: 9000,
     allowedHosts: true,
     // 禁用默认的 host 检查，这在云平台环境中有时是必要的
-    strictPort: true,
+    strictPort: false, // 运行端口占用，自动切换端口
   },
   preview: {
     host: '0.0.0.0',
     port: port,
     allowedHosts: true,
-    strictPort: true,
+    strictPort: false,
   },
 })
